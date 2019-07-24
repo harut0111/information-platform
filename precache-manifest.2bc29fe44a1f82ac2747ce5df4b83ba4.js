0a41b16ae6506f108767c3e9ae1a30a72dd8fc74
Harut Muqoyan <hmuqoyan@gmail.com>
2019-07-24 11:16:13 +0400

    Updates

---
+++ precache-manifest.2bc29fe44a1f82ac2747ce5df4b83ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da220623d431bc5026995b76a009bfe2",
+    "url": "/information-platform/index.html"
+  },
+  {
+    "revision": "7a11e3927f6bc2a9d5ef",
+    "url": "/information-platform/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/information-platform/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7a11e3927f6bc2a9d5ef",
+    "url": "/information-platform/static/js/main.33590716.chunk.js"
+  },
+  {
+    "revision": "5885ee3f2d844ab95822",
+    "url": "/information-platform/static/js/runtime~main.53dfa125.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/information-platform/static/media/logo.5d5d9eef.svg"
+  }
+]);
