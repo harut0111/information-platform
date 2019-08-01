242315b74b007a75b12b4b4b723598dcaa655ad2
Edgar Hovhannisyan <edgar.hovhannisyan08@gmail.com>
2019-08-01 11:19:23 +0400

    created new Component 'Main'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from "./js/components/Header";
 import Footer from "./js/components/Footer";
-import MainLeftSide from "./js/components/MainLeftSide";
-import MainRightSide from "./js/components/MainRightSide";
+import Main from "./js/components/Main";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <main>
-        <MainLeftSide />
-        <MainRightSide />
-      </main>
+      <Main />
       <Footer />
     </div>
   );
