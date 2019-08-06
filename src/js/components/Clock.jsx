0b4ddc2f0b746c3abad886c3517a59b9203dfeff
ArmanShirinyan <mr.shirinyan2019@gmail.com>
2019-08-07 02:29:58 +0400

    add clock components with color changies

---
+++ src/js/components/Clock.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import "./styles/clock.css";
+
+class Clock extends Component {
+
+
+  constructor() {
+    super()
+this.state={time:new Date()}
+  }
+
+  
+  currentTime()
+  {
+    this.setState({
+      time: new Date()
+    })
+  }
+  componentWillMount()
+  {
+setInterval(()=>this.currentTime(),1000)
+
+  }
+
+
+  render() {
+    
+ setInterval(
+   function colorGen() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    
+    document.getElementById("clock").style.color = "rgb(" + r + "," + g + "," + 0 + ")";
+  },1000);
+    
+    return (
+      
+      <div id='clock'>
+        {this.state.time.toLocaleTimeString()}
+      </div>
+    )
+  }
+  
+}
+
+export default Clock;
