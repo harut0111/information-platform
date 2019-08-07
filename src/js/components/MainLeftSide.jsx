2944c809d109c033810f435cd43c8058ac0eac45
ArmanShirinyan <mr.shirinyan2019@gmail.com>
2019-08-07 11:22:04 +0400

    main left side with firebase

--- src/js/components/MainLeftSide.jsx
+++ src/js/components/MainLeftSide.jsx
@@ -1,21 +1,51 @@
-import React from "react";
-import "./styles/mainLeftSide.css";
 
-export default function MainLeftSide() {
+import "./styles/mainLeftSide.css";
+import firebase from "../configs/FireBase";
+import "firebase/firestore";
+ 
+import React, { Component } from 'react';
+
+export class MainLeftSide extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          groupList: [],
+        }
+      }
+      componentDidMount() {
+    
+        this.db = firebase.firestore();
+        this.db.collection("Group").get().then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+             this.setState(state => ({
+              groupList: state.groupList.concat({
+                id: doc.id,
+                value: doc.data().name,
+              }),
+            }));
+          });
+        });
+      }
+      render() {
+      
+        const { groupList } = this.state;
+        const items = groupList.map(item => {
+          return (
+            <div className='groups' key= {item.id} name={item.value}><h2> {item.value} </h2></div>
+          )
+        })
+    
     return (
         <div className="leftSide">
             <div className="leftSideWrapper">
                 <h1>GROUPS OF OUR COMPANY</h1>
                 <div className="groupsContainer">
-                    <div className="groups"><h2>FRONT END</h2></div>
-                    <div className="groups"><h2>BACK END</h2></div>
-                    <div className="groups"><h2>FULL STACK</h2></div>
-                    <div className="groups"><h2>WEB DESIGN</h2></div>
-                    <div className="groups"><h2>QA</h2></div>
-                    <div className="groups"><h2>HR</h2></div>
-                    <div className="groups"><h2>CEO</h2></div>
-                </div>
+                    {items} 
+                 </div>
             </div>
-        </div>
+            </div>
     )
 }
+}
+
+export default MainLeftSide
